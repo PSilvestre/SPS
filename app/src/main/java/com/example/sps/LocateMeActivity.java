c5678d3d0bb3552a03ba11323e463e8a22c1ed84
soqueroprogramar <joao.morais.jumper@hotmail.com>
2019-05-01 19:48:46 +0200

    Added Sensor Sensing

--- app/src/main/java/com/example/sps/LocateMeActivity.java
+++ app/src/main/java/com/example/sps/LocateMeActivity.java
@@ -1,8 +1,16 @@
 package com.example.sps;
 
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,13 +21,15 @@ import android.widget.TextView;
 import com.example.sps.activity_recognizer.ActivityRecognizer;
 import com.example.sps.activity_recognizer.FloatTriplet;
 import com.example.sps.activity_recognizer.SubjectActivity;
+import com.example.sps.data_collection.DataCollectOnClickListener;
 import com.example.sps.data_collection.DataCollectionActivity;
 import com.example.sps.localization_method.LocalizationMethod;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
-public class LocateMeActivity extends AppCompatActivity {
+public class LocateMeActivity extends AppCompatActivity implements SensorEventListener {
 
     private Button initialBeliefButton;
     private Button locateMeButton;
@@ -31,6 +41,9 @@ public class LocateMeActivity extends AppCompatActivity {
     private ActivityRecognizer activityRecognizer;
     private LocalizationMethod localizationMethod;
 
+    private SensorManager sensorManager;
+    private Sensor accelerometer;
+    private List<FloatTriplet> accSensor;
 
     List<Float> cellProbabilities;
     int numCells;
@@ -61,9 +74,23 @@ public class LocateMeActivity extends AppCompatActivity {
             public void onClick(View view) {
                 List<ScanResult> scanResults = null;
                 int cell_guess = localizationMethod.computeLocation(scanResults);
-                List<FloatTriplet> accelorometerData = null;
-                SubjectActivity activity = activityRecognizer.recognizeActivity(accelorometerData);
+                accSensor = new LinkedList<>();
 
+                // Set the sensor manager
+                sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+                // if the default accelerometer exists
+                if (sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null) {
+                    // set accelerometer
+                    accelerometer = sensorManager
+                            .getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+                    // register 'this' as a listener that updates values. Each time a sensor value changes,
+                    // the method 'onSensorChanged()' is called.
+                    sensorManager.registerListener((SensorEventListener) view.getContext(), accelerometer,
+                            SensorManager.SENSOR_DELAY_NORMAL);
+                } else {
+                    // No accelerometer!
+                }
 
             }
         });
@@ -73,8 +100,28 @@ public class LocateMeActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent((Activity) view.getContext(), DataCollectionActivity.class);
                 startActivity(intent);
+
+
             }
         });
+
+
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        // get the the x,y,z values of the accelerometer
+        accSensor.add(new FloatTriplet(event.values[0], event.values[1], event.values[2]));
+        if ( accSensor.size() == 10 ) {
+            SubjectActivity activity = activityRecognizer.recognizeActivity(accSensor);
+            sensorManager.unregisterListener(this);
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+        //Do everything!!!
     }
 
     protected void setInitialBelief(){
